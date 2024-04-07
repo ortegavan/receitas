c5b8a13b906a993eea52b9de49f8962fe87d5a6c
Vanessa Ortega <ortegavan@live.com>
2024-04-07 20:26:14 -0300

    feat(home): feito header

---
+++ src/app/routes/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../ui/header/header.component';
+
+@Component({
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [HeaderComponent],
+})
+export class HomeComponent {}
