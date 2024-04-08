d9cd0fccd2e7f097a589b2ced6f47b35f6118b17
Vanessa Ortega <ortegavan@live.com>
2024-04-08 09:43:52 -0300

    feat(receitas): criado card de receitas

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -2,13 +2,14 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../ui/header/header.component';
 import { RecipeService } from '../../services/recipe.service';
+import { RecipeComponent } from '../../ui/recipe/recipe.component';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [HeaderComponent, CommonModule],
+    imports: [HeaderComponent, CommonModule, RecipeComponent],
 })
 export class HomeComponent {
     recipeService = inject(RecipeService);
--- src/app/ui/recipe/recipe.component.ts
+++ src/app/ui/recipe/recipe.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Recipe } from '../../models/recipe.model';
 
 @Component({
-  selector: 'app-recipe',
-  standalone: true,
-  imports: [],
-  templateUrl: './recipe.component.html',
-  styleUrl: './recipe.component.css'
+    selector: 'app-recipe',
+    standalone: true,
+    imports: [],
+    templateUrl: './recipe.component.html',
+    styleUrl: './recipe.component.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RecipeComponent {
-
+    @Input({ required: true }) recipe!: Recipe;
 }
